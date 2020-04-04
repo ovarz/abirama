d2a34373b491e52ed45ab7fe2e2b21a2f0df7996
ovarz <ovarz_03@yahoo.com>
2020-04-04 19:54:16 +0700

    j

--- js/base.js
+++ js/base.js
@@ -114,4 +114,5 @@ $(window).on('load', function(){
   refresh_up();
   scroll_reveal_show();
   $(".se-pre-con").fadeOut("slow");
+  $(".contact-map-box").html('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.5407416628086!2d106.90391861529494!3d-6.192148262391275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4c9865adfe7%3A0x681f6df6627ccd32!2sPintu%20Masuk%20Kawasan%20JIEP%20PULO%20GADUNG!5e0!3m2!1sen!2sid!4v1585909143396!5m2!1sen!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>');
 }); 
