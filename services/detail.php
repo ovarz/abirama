5c1fe22ad0f2fb78ec8dc26f4af6cf8ec468f0c1
ovarz <ovarz_03@yahoo.com>
2020-04-05 10:55:53 +0700

    a

---
+++ services/detail.php
@@ -0,0 +1,32 @@
+<?php $level='two'; $page='services'; require ('../inc/top.php')?>
+<?php require ('../inc/header.php')?>
+<div class="site-container">
+
+  <section class="base-container content_center" id="services">
+	<span>
+	  <h4 class="section-title">Kalibrasi</h4>
+	  <div class="article-box">
+	    <div class="article-image flex_ori">
+		  <img alt="image" src="img/sample/sample-detail.jpg" />
+		</div>
+		<p><b>Lorem ipsum dolor sit amet</b>, consectetur adipiscing elit. Phasellus et nibh et nisi semper dignissim. Nam nulla justo, vestibulum bibendum dolor placerat, sagittis euismod mauris.</p>
+		<ul>
+		  <li>Nullam bibendum massa varius risus ultricies malesuada</li>
+		  <li>Duis gravida tempor vehicula. Nullam leo ligula</li>
+		  <li>facilisis in est sit amet, commodo placerat nisl</li>
+		</ul>
+		<p>Nullam vel arcu at felis eleifend efficitur. <i>Aliquam mollis dolor eu nibh facilisis euismod</i>. Praesent semper odio sit amet ultrices hendrerit. Duis ultricies eleifend ligula quis tincidunt. Pellentesque ut mi vel libero auctor gravida. Nullam sit amet felis eros.</p>
+		<p>Suspendisse vel sem ac turpis suscipit bibendum ut et dolor. <strong>Cras varius auctor ex molestie pellentesque</strong>. Donec nec justo ex. Aliquam ut nisl vitae ante auctor ultrices quis non tellus.</p>
+		<p>Aenean non commodo dui, non finibus magna. Vivamus condimentum tristique eros vel maximus. Maecenas molestie varius pellentesque.</p>
+		<ol>
+		  <li>Nullam bibendum massa varius risus ultricies malesuada</li>
+		  <li>Duis gravida tempor vehicula. Nullam leo ligula</li>
+		  <li>facilisis in est sit amet, commodo placerat nisl</li>
+		</ol>
+		<p>Nam sit amet justo consectetur, facilisis libero sit amet, <em>fringilla turpis</em>. Morbi quam ante, euismod vel rutrum tincidunt, efficitur a leo. Suspendisse mollis tortor id mi condimentum lacinia.</p>
+	  </div>
+	</span>
+  </section>
+
+</div>
+<?php require ('../inc/footer.php')?>
